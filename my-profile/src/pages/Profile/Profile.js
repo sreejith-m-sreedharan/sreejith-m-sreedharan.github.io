e2c8e4f32c7b0f3fc0d4fdaab7662867415e9bd9
Sreejith MS <sreejith.m.sreedharan@gmail.com>
2023-03-29 14:44:12 +0530

    added fix for router in github page

--- my-profile/src/pages/Profile/Profile.js
+++ my-profile/src/pages/Profile/Profile.js
@@ -1,15 +1,17 @@
 import * as classes from './Profile.css';
 import { useNavigate } from 'react-router-dom';
-
+import { useEffect } from 'react';
 export default function Profile(){
     const navigateTo = useNavigate();
     let path = localStorage.getItem('path');
-  
-    if(path) {
-      localStorage.removeItem('path');
-      navigateTo(path)
-    }
+    useEffect(()=>{
+        if(path) {
+            localStorage.removeItem('path');
+            navigateTo(path);
+          }
+    },[path]);
     
+ 
     return <div className='container'> 
     <div className='header'>
         My Profile
